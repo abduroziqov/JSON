3c0e5a0d8d3466b8dffcef2d6134090a6f6ad44f
QUVONCHBEK ABDUROZIQOV <75139080+abduroziqov@users.noreply.github.com>
2023-06-19 15:04:56 +0500

    Add project files.

---
+++ JSON/Company.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace JSON
+{
+    internal class Company
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+       public List<Car> Cars { get; set; }
+    }
+}
---
+++ JSON/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+
+namespace JSON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Company company = new Company
+            {
+                Id = 1,
+                Name = "Anvar",
+                Cars = new List<Car> {
+                new Car { Id = 1, Name = "BMW ", Price = 120000 },
+                new Car { Id = 2, Name = "MERS ", Price = 125000 }
+                }
+            };
+
+            string json = JsonSerializer.Serialize(company);
+            Console.WriteLine(json);
+
+            Console.ReadLine();
+
+            string path = Directory.GetCurrentDirectory();
+            path += "\\company.json";
+
+            string json2 = JsonSerializer.Serialize(company);
+
+            if (!File.Exists(path))
+            {
+                File.Create(path).Close();
+
+                File.WriteAllText(path, json2);
+            }
+        }
+    }
+}   
